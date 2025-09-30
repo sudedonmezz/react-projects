9c08ce336fb53f5422fc10175ba195832cb70b3c
sudedonmezz <donmezsude41@gmail.com>
2025-09-30 14:24:32 +0300

    update links to match the router

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
       <Routes>
         <Route index element={<HomePage/>}/>
-        <Route path="/checkout.html" element={<Checkout/>}/>
+        <Route path="/checkout" element={<Checkout/>}/>
         
       </Routes>
      
